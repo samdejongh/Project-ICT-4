5026d1dd3e07eab62ac6d16b8ca0be2ef589bf2f
Sennevds <senne.vandesompele@student.artesis.be>
2013-03-18 17:32:13 +0100

    Begin project

---
+++ src/WpfApplication10/WpfApplication10/InitExcel.cs
@@ -0,0 +1,56 @@
+﻿/*
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Office.Interop.Excel;
+namespace WpfApplication10
+{
+    class InitExcel
+    {
+        public Microsoft.Office.Interop.Excel.Application xlApp;
+        public Microsoft.Office.Interop.Excel.Workbook xlWorkBook;
+        public Microsoft.Office.Interop.Excel.Worksheet xlWorkSheet;
+        public Microsoft.Office.Interop.Excel.Range range;
+        public object misValue = System.Reflection.Missing.Value;
+
+        public void Init()
+        {
+            xlApp = new Microsoft.Office.Interop.Excel.Application();
+            xlWorkBook = xlApp.Workbooks.Open("email.xls", 0, false, 5, "", "", false, Microsoft.Office.Interop.Excel.XlPlatform.xlWindows, "", true, false, 0, true, false, false);
+
+            xlWorkSheet = (Microsoft.Office.Interop.Excel.Worksheet)xlWorkBook.Worksheets.get_Item(1);
+            range = xlWorkSheet.UsedRange;
+        }
+
+        public void Close()
+        {
+            xlWorkBook.Close(true, misValue, misValue);
+            xlApp.Quit();
+
+            releaseObject(xlWorkSheet);
+            releaseObject(xlWorkBook);
+            releaseObject(xlApp);
+        }
+
+        private void releaseObject(object obj)
+        {
+            try
+            {
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(obj);
+                obj = null;
+            }
+            catch (Exception ex)
+            {
+                obj = null;
+                MessageBox.Show("Het object kan niet weergegeven worden " + ex.ToString());
+            }
+            finally
+            {
+                GC.Collect();
+            }
+        }
+    }
+}
+*/
---
+++ src/WpfApplication10/WpfApplication10/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+
+namespace WpfApplication10
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void rFolder_Click(object sender, RoutedEventArgs e)
+        {
+            FolderBrowserDialog folder = new FolderBrowserDialog();
+            folder.ShowDialog();
+            if (folder.SelectedPath != string.Empty)
+            {
+                string[] files = Directory.GetFiles(folder.SelectedPath);
+                foreach (var file in files)
+                {
+                    debug.Items.Add(file);
+                }
+            }
+        }
+
+        private void Debug_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            debug.SelectedItems.ToString();
+        }
+    }
+}
