a1543fbbba185069115fce20575634e6cf9b3544
brent proost <brent_proost@hotmail.com>
2013-03-20 21:09:30 +0100

    test

--- src/WpfApplication10/WpfApplication10/MainWindow.xaml.cs
+++ src/WpfApplication10/WpfApplication10/MainWindow.xaml.cs
@@ -1,24 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Reflection;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Forms;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using Microsoft.Win32;
+using GemBox.Spreadsheet;
+using Microsoft.Office.Interop.Excel;
 using MessageBox = System.Windows.MessageBox;
-using Path = System.IO.Path;
-using OfficeOpenXml;
-using FileDialog = System.Windows.Forms.FileDialog;
 using SaveFileDialog = Microsoft.Win32.SaveFileDialog;
 
 namespace WpfApplication10
@@ -26,12 +15,9 @@ namespace WpfApplication10
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
-        int countingtitle = 0; //zodat die datum enzo er maar 1 keer opstaat
-        string[] words;
-        int teller;
-        List<string> ls = new List<string>();
+        private int _countingtitle; //zodat die datum enzo er maar 1 keer opstaat
 
         public MainWindow()
         {
@@ -47,53 +33,55 @@ namespace WpfApplication10
                 string[] files = Directory.GetFiles(folder.SelectedPath);
                 foreach (var file in files)
                 {
-                    debug.Items.Add(file);//dit werk wel maar ni voor de volgende stap
+                    debug.Items.Add(file); //dit werk wel maar ni voor de volgende stap
                     // ik heb effe zitte sukkelen me nen databinder naar de listbox maar da                                                                                                      was zo veel gezever da
                     //ik da heb opgegeven
                 }
             }
         }
+
         private void Debug_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             debug2.Items.Clear(); //anders blijft er meer en meer bijkomen bij een verandering
-            countingtitle = 0;
+            _countingtitle = 0;
 
             for (int i = 0; i < debug.SelectedItems.Count; i++)
             {
-                using (StreamReader reader = new StreamReader(Convert.ToString(debug.SelectedItems[i]))) //vanaf hier gewoon simpelweg elke selecteditem in die 2delistbox zetten (debug2)
+                using (StreamReader reader = new StreamReader(Convert.ToString(debug.SelectedItems[i])))
+                    //vanaf hier gewoon simpelweg elke selecteditem in die 2delistbox zetten (debug2)
                 {
                     string line;
                     while ((line = reader.ReadLine()) != null)
                     {
-                        if (countingtitle == 0 || line.ToString().Contains("Date") == false)
+                        if (_countingtitle == 0 || line.Contains("Date") == false)
                         {
                             debug2.Items.Add(line);
-                            countingtitle = 1; //zo komt er dus maar 1 keer die datum in
+                            _countingtitle = 1; //zo komt er dus maar 1 keer die datum in
                         }
                     }
                 }
             }
         }
 
-        
 
-        
 
-       
+
+
+
         private void sAll_Click(object sender, RoutedEventArgs e)
         {
             debug.SelectAll();
         }
 
-        private void MenuItem_OnClick(object sender, RoutedEventArgs e)//exit button van menu
+        private void MenuItem_OnClick(object sender, RoutedEventArgs e) //exit button van menu
         {
             MessageBoxResult dialogResult = MessageBox.Show(@"Do you want to quit?", @"Sure", MessageBoxButton.YesNo);
-            
+
             //vraag ineen message box
 
             if (dialogResult == MessageBoxResult.Yes)
             {
-                Close();//afsluiten van het programma
+                Close(); //afsluiten van het programma
             }
 
 
@@ -102,13 +90,13 @@ namespace WpfApplication10
         private void About(object sender, RoutedEventArgs e)
         {
             MessageBox.Show(
-                "This software is coded by Brent and Senne \nIf you want to use this software please contact us",
+                "This software is coded by Brent Proost and Senne van de Sompele \nIf you want to use this software please contact us",
                 "About");
         }
 
         private void Export_Click(object sender, RoutedEventArgs e)
         {
-            string s=" ";
+            string s = " ";
             string t = " ";
             SaveFileDialog dlg = new SaveFileDialog();
             dlg.Filter = "csv files (*.csv)|*.csv";
@@ -127,7 +115,21 @@ namespace WpfApplication10
                     myOutputStream.WriteLine(t);
                 }
                 myOutputStream.Close();
+                ConvertCSV();
             }
+        }
+
+        private void ConvertCSV()
+        {
+            SpreadsheetInfo.SetLicense("FREE-LIMITED-KEY");
+            // Create new CSV file. 
+            var csvFile = new ExcelFile();
+
+            // Load data from CSV file. 
+            csvFile.LoadCsv(@"C:\Users\brent\Documents\SmartGit\src\WpfApplication10\WpfApplication10\bin\Debug\Myfile.csv", CsvType.CommaDelimited);
+
+            // Save CSV file to XLS file. 
+            csvFile.SaveXls(@"C:\Users\brent\Documents\SmartGit\src\WpfApplication10\WpfApplication10\bin\Debug\Converted.xls"); 
         }
     }
 }
