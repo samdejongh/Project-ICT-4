3baf0352ede09c186bc1e6db5423019749fc6e03
Sennevds <senne.vandesompele@student.artesis.be>
2013-03-22 14:57:45 +0100

    De manier waarop de gps wordt aangeroepen is verandert en overal comments bijgezet wat er juist gebeurd

--- testproject/LocalFiles/js/Maps.js
+++ testproject/LocalFiles/js/Maps.js
@@ -1,68 +1,81 @@
 
+var lati = 65.012482;
+var longi = 25.472181;
+var map;
+var marker;
+var speed;
+function initialize() {//laad de map layout en roep de functie startGPS aan
+	startGPS();
+	var mapOptions = {
+		center : new google.maps.LatLng(lati, longi),
+		zoom : 10,
+		mapTypeControl : true,//optie om de zoom buttons te laten zien
+		mapTypeControlOptions : {
+			style : google.maps.MapTypeControlStyle.DEFAULT//de panning buttons(staan af momenteel)
+		},
+		zoomControl : true,
+		zoomControlOptions : {
+			style : google.maps.ZoomControlStyle.SMALL//de zoom buttons staan aan
+		},
+		streetViewControl : false,
+		mapTypeId : google.maps.MapTypeId.ROADMAP//welk type van map je laat zien
+	};
+	map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+	document.getElementById("debug").innerHTML = "success initialize";//voor debugging
 
-	var lati = 65.012482;
-	var longi = 25.472181;
-	var map;
-	var marker;
-	function initialize() {
-		//mosync.bridge.send(["Custom", "Vibrate", "500"]);
-		mosync.bridge.send(["Custom", "Start"]);
-		var mapOptions = {
-			center : new google.maps.LatLng(lati, longi),
-			zoom : 10,
-			mapTypeControl : true,
-			mapTypeControlOptions : {
-				style : google.maps.MapTypeControlStyle.DEFAULT
-			},
-			zoomControl : true,
-			zoomControlOptions : {
-				style : google.maps.ZoomControlStyle.SMALL
-			},
-			streetViewControl : false,
-			mapTypeId : google.maps.MapTypeId.ROADMAP
-		};
-		map = new google.maps.Map(document.getElementById("map_canvas"),
-				mapOptions);
-		document.getElementById("debug").innerHTML = "success initialize";
-		
-	}
-	function MapUpdate() {
-		document.getElementById("debug").innerHTML = "success1";
-		if (marker != null) {
-			marker.setMap(null);
-		}
-		document.getElementById("debug").innerHTML = "success2";
-		var myLatlng = new google.maps.LatLng(lati, longi);
-		map.setCenter(myLatlng);
-		map.setZoom(16);
-		document.getElementById("debug").innerHTML = "success3";
-		marker = new google.maps.Marker({
-			position : myLatlng,
-			map : map,
-			title : "You"
+}
+function startGPS() {
+	alert("gps loaded");//voor debugging
+	var element = document.getElementById('geolocation');//voor debugging
+	if (navigator.geolocation) {//als het toestel dit ondersteunt
+		navigator.geolocation.watchPosition(onSuccess, onError, {//start gps
+			enableHighAccuracy : true,//dit zorgt er voor dat gps wordt gebruikt ipv cell id
+			maximumAge : 30000,//tijd in ms voor er een error wordt gestuurd als het te lang duurt
+			timeout : 27000//de tijd tussen de verschillende oproepen om een nieuwe locatie te vinden
 		});
-		document.getElementById("debug").innerHTML = "success4";
-
-	}
-	// Wait for Cordova to load
-	//
-
-	// onSuccess Geolocation
-	//
-	function onSuccess(lat, lon) {
-		var element = document.getElementById('geolocation');
-		element.innerHTML = 'Latitude: ' + lat + '<br />' + 'Longitude: ' + lon
-				+ '<br />';
-		lati = lat;
-		longi = lon;
-		MapUpdate();
-
+		element.innerHTML = "searching satelites"
+	} else {
+		element.innerHTML = "Geolocation is not supported by this browser.";
 	}
 
-	function onError(error) {
-		alert('code: ' + error.code + '\n' + 'message: ' + error.message + '\n');
+}
+function onSuccess(position) {// als er succesvol een nieuwe locatie is gevonden
+	var element = document.getElementById('geolocation');
+	element.innerHTML = 'Latitude: ' + position.coords.latitude + '<br />'
+			+ 'Longitude: ' + position.coords.longitude + '<br />';//weergeven van de positie op de pagina
+	lati = position.coords.latitude;
+	longi = position.coords.longitude;
+	speed = position.coords.speed;
+	document.getElementById("speed").innerHTML=speed+'m/s'//weergeven van de snelheid
+	MapUpdate();//roep de functie mapUpdate aan
+
+}
+
+function MapUpdate() {//de kaart centreren naar de nieuwe locatie
+	document.getElementById("debug").innerHTML = "success1";
+	if (marker != null) {//de vorige marker verwijderen anders blijven er steeds nieuwe komen
+		marker.setMap(null);
 	}
-	function vibrate()
-	{
-		mosync.bridge.send(["Custom", "Vibrate", "500"]);
-	}
+	document.getElementById("debug").innerHTML = "success2";
+	var myLatlng = new google.maps.LatLng(lati, longi);
+	map.setCenter(myLatlng);//de locatie waar de map zich moet centreren
+	map.setZoom(16);
+	document.getElementById("debug").innerHTML = "success3";
+	marker = new google.maps.Marker({
+		position : myLatlng,
+		map : map,
+		title : "You"
+	});
+	document.getElementById("debug").innerHTML = "success4";
+
+}
+
+
+
+
+function onError(error) {
+	alert('code: ' + error.code + '\n' + 'message: ' + error.message + '\n');
+}
+function vibrate() {
+	mosync.bridge.send([ "Custom", "Vibrate", "500" ]);
+}
--- testproject/main.cpp
+++ testproject/main.cpp
@@ -36,14 +36,12 @@ using namespace Wormhole;
 class MyMoblet: public HybridMoblet {
 public:
 	MyMoblet() {
+
 		showPage("index.html");
-		addMessageFun("Start", (FunTable::MessageHandlerFun) &MyMoblet::start);
 		addMessageFun("Change", (FunTable::MessageHandlerFun) &MyMoblet::orent);
 		addMessageFun("Vibrate", (FunTable::MessageHandlerFun) &MyMoblet::vibrate);
 	}
-	void start(Wormhole::MessageStream& message) {
-		int res = maLocationStart();
-	}
+
 	void orent(Wormhole::MessageStream& message) {
 		MAUtil::String (url) = MAUtil::String(message.getNext());
 		showPage(url);
@@ -53,38 +51,23 @@ public:
 		maVibrate(duration);
 	}
 
-	void customEvent(const MAEvent& event) {
-		if (event.type == EVENT_TYPE_LOCATION) {
-			MALocation& loc = *(MALocation*) event.data;
-
-			if (loc.lon < -180.0 || loc.lon > 180.0 || loc.lat < -90.0
-					|| loc.lat > 90.0) {
-				printf("invalid lat or lon\n");
-			} else {
-				printf("%i %.8g %.8g %.4g %.4g %.4g\n", loc.state, loc.lat,
-						loc.lon, loc.horzAcc, loc.vertAcc, loc.alt);
-				// number to be converted to a string
-				String latstr = doubleToString(loc.lat);
-				String lonstr = doubleToString(loc.lon);
-				char lati[200];
-				sprintf(lati, "onSuccess('%f','%f')", loc.lat,
-						loc.lon);
-				callJS(lati);
-				maLocationStart();
-			}
-
-		} else if (event.type == EVENT_TYPE_LOCATION_PROVIDER) {
-			const char *strings[] = { "AVAILABLE", "TEMPORARILY_UNAVAILABLE",
-					"OUT_OF_SERVICE" };
-
-			printf("gps provider: %s\n", strings[event.state - 1]);
-			callJS("alert(strings[event.state - 1])");
-
-		} else {
-			printf("custom event %i\n", event.type);
-			callJS("alert(event.type)");
-		}
+	void MyMoblet::keyPressEvent(int keyCode, int nativeCode)
+	{
+	  if (MAK_BACK == keyCode)
+	  {
+	    String url=getWebView()->getURL();
+	    url=url.substr(url.length()-10,10);
+	    if (url=="index.html")
+	    {
+	      closeEvent();
+	    }
+	    else
+	    {
+	      callJS("history.back()");
+	    }
+	  }
 	}
+
 
 };
 extern "C" int MAMain() {
