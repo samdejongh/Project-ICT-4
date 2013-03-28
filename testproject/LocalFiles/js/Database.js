4fd18d8594a63c716ac6c3992143dacbc67928b9
Sennevds <senne.vandesompele@student.artesis.be>
2013-03-28 21:01:43 +0100

    Comments toegevoegd met uitleg

--- testproject/LocalFiles/js/Database.js
+++ testproject/LocalFiles/js/Database.js
@@ -5,9 +5,9 @@ $.ajax({
 					type:"GET",
 					//url:"http://b87861e85ebd401b9ccbafd684d5167b.cloudapp.net/oulu.svc/members",
 					//url:"http://localhost/test.txt",
-					url:"http://circant.be/project/test_data.php",
-					data: { nr : nr,lat : lati,lon:longi,dis:dist },
-					success: function(data,status)
+					url:"http://circant.be/project/test_data.php",//link naar php bestand hier wordt je data naar toe gezonden
+					data: { nr : nr,lat : lati,lon:longi,dis:dist },//php file kan 4 variabelen ontvangen de trip nummer de lati en longi en de totale afstand die gelopen is
+					success: function(data,status)//als het gelukt is dit uitvoeren
 					{
 						alert(status);
 //						var jsArr = JSON.parse(data);
