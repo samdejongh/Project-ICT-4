56aa01a307c740226ccb448e5430e911de8c3984
Sennevds <senne.vandesompele@student.artesis.be>
2013-05-23 09:48:37 +0200

    latest version

--- Orienteering_Guide/LocalFiles/js/Database.js
+++ Orienteering_Guide/LocalFiles/js/Database.js
@@ -16,25 +16,12 @@ function DBconnect()
 }
 function sendtodbpoints(id,nr,lat,lon)
 {
-	$.ajax({
-		type:"GET",
-		//url:"http://b87861e85ebd401b9ccbafd684d5167b.cloudapp.net/oulu.svc/members",
-		//url:"http://localhost/test.txt",
-		url:"http://roge-senne.rhcloud.com/project/test_data.php",//link naar php bestand hier wordt je data naar toe gezonden
-		data: { ID : id, pointnr : nr, lat:lat, lon:lon },//php file kan 4 variabelen ontvangen de trip nummer de lati en longi en de totale afstand die gelopen is
-		success: function(data,status)//als het gelukt is dit uitvoeren
-		{
-			//alert(status);
-//			var jsArr = JSON.parse(data);
-//			for(var i=0;i<jsArr.length;i++)
-//			{
-//				$("ul").append("<li>"+ jsArr[i].trip+"</li>");
-//				
-//				}
-		},
-		error: function(a,b,c){alert("error");}
+	$.getJSON("http://roge-senne.rhcloud.com/project/",data(result));
 	
-	});
+}
+function data (result)
+{
+	
 }
 function sendtodb(nr,lati,longi,dist)
 {
